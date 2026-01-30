4a962ec158153943dd0897be2fae01c45370f6bf
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2026-01-29 21:44:29 -0300

    Fim config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="flex h-screen items-center justify-center">
+      <Button>Funcionou</Button>
     </h1>
-  )
+  );
 }
